edb322ba352ea52f87f173979020fd90338ec18b
Abin Pal <apal4@sapient.com>
2017-01-18 19:15:07 +0530

    removed superagent and started using request

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var read = require('read');
 var config = require('./config');
-var exportPage = require('./lib/exportPage');
+var requestAPI = require('./lib/requestAPI');
+var async = require('async');
 
-var username, password, confPageId;
+var username, password, confPageId, url;
 
 read({ prompt: 'Username: ', silent: false }, function(er, username) {
   username = username;
@@ -10,10 +11,29 @@ read({ prompt: 'Username: ', silent: false }, function(er, username) {
     password = password;
     read({ prompt: 'Confluence Page ID: ', silent: false }, function(er, confPageId) {
       confPageId = confPageId;
+      url = config.confConfig.apiPath + "/content/" + confPageId + "?expand=body.storage,version";
       updateConfig(username, password);
-      exportPage(confPageId,config,function(err,data){
-        if(err) console.log(err.response.res.statusMessage);
-        else console.log('---data from export----',data);
+      requestAPI(confPageId, config, url, 'false', '', function(err,data){
+        if(err) console.log(err);
+        else {
+          //console.log('---data of parent page----',data);
+          var attachmentsURL = config.confConfig.apiPath + "/content/" + confPageId + "/child/attachment";
+          requestAPI(confPageId, config, attachmentsURL, 'false', '', function(err,data1){
+            //console.log('---data of attachments page----',data1);
+            data1 = JSON.parse(data1);
+            if(data1.results.length > 0){
+              async.eachSeries(data1.results, function iteratee(result, callback) {
+              if (result) {
+                console.log(result._links.download);
+                requestAPI(confPageId, config, result._links.download, true, result.title, function(err,data2){
+                  console.log('---data of attachments page----',data2);
+                  callback();
+                });
+              }
+              });
+            }
+          });
+        }
       });
     });
   });
