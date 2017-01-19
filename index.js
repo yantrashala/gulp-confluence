26eb8faf492894080075e065d0e99ce350ab84f3
Abin Pal <apal4@sapient.com>
2017-01-19 15:29:01 +0530

    exported html file

--- index.js
+++ index.js
@@ -13,20 +13,21 @@ read({ prompt: 'Username: ', silent: false }, function(er, username) {
       confPageId = confPageId;
       url = config.confConfig.apiPath + "/content/" + confPageId + "?expand=body.storage,version";
       updateConfig(username, password);
-      requestAPI(confPageId, config, url, 'false', '', function(err,data){
+      requestAPI(confPageId, config, url, false, '', function(err,data){
         if(err) console.log(err);
         else {
-          //console.log('---data of parent page----',data);
+          data = JSON.parse(data);
+          requestAPI(confPageId, config, url, true, data.title +'.html', function(err,data11){
+            console.log(data11);
+          });
           var attachmentsURL = config.confConfig.apiPath + "/content/" + confPageId + "/child/attachment";
           requestAPI(confPageId, config, attachmentsURL, 'false', '', function(err,data1){
-            //console.log('---data of attachments page----',data1);
             data1 = JSON.parse(data1);
             if(data1.results.length > 0){
               async.eachSeries(data1.results, function iteratee(result, callback) {
               if (result) {
-                console.log(result._links.download);
                 requestAPI(confPageId, config, result._links.download, true, result.title, function(err,data2){
-                  console.log('---data of attachments page----',data2);
+                  console.log(data2);
                   callback();
                 });
               }
