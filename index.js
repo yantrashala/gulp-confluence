1df60fa6f3f209e393d2c5ed645d68ab864738f1
Abin Pal <apal4@sapient.com>
2017-02-03 12:04:26 +0530

    updated code for sanitizing filenames

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var requestAPI = require('./lib/requestAPI');
 var async = require('async');
 var chalk = require('chalk');
 
-var username, password, confPageId, url, childUrl;
+var username, password, confPageId;
 var inputColor = chalk.cyan;
 var dataColor = chalk.green;
 var attachmentColor = chalk.magenta;
@@ -17,7 +17,7 @@ read({ prompt: inputColor('Username: '), silent: false }, function(er, uname) {
     read({ prompt: inputColor('Confluence Page ID: '), silent: false }, function(er, id) {
       confPageId = id;
       updateConfig(username, password);
-      loop();
+      loop(confPageId);
     });
   });
 });
@@ -27,71 +27,88 @@ function updateConfig(username, password){
   config.confConfig.password  = password;
 }
 
-function loop(){
-  setTimeout(getAPIData(saveMD), 1000);
-  setTimeout(getAttachments, 1000);
-  setTimeout(callChildPages, 1000);
+function loop(confPageId){
+  getAPIData(confPageId, saveMD, function(err, data){
+    if (err) console.log(errColor(err));
+    else {
+      console.log(dataColor(data));
+      getAttachments(confPageId,function(err, data){
+        if(err) {
+          console.log(errColor(err));
+        }
+        else{
+          callChildPages(confPageId);
+        }
+      });
+    };
+  });
 }
 
-function getAPIData(callMD){
-  url = config.confConfig.apiPath + "/content/" + confPageId + "?expand=body.storage,version";
+function getAPIData(confPageId, callMD, callback){
+  var url = config.confConfig.apiPath + "/content/" + confPageId + "?expand=body.storage,version";
   requestAPI(confPageId, config, url, false, '', false, function(err,data){
     if(err) {
-      console.log(errColor(err));
-      //return err;
+      callback(err, null);
     }
     else {
       data = JSON.parse(data);
-      setTimeout(callMD(data.title), 2000);
+      callMD(confPageId, data.title, url, function(err,data){
+        if (err) callback(err, null);
+        else callback(null, data);
+      });
     }
   });
 }
 
-function saveMD(title){
+function saveMD(confPageId, title, url, callback){
   requestAPI(confPageId, config, url, false, title +'.md', true, function(err,data){
-    if(err) console.log(errColor(err));
+    if(err) {
+      callback(err, null);
+    }
     else {
-      console.log(dataColor(data));
+      callback(null, data);
     }
   });
 }
 
-function getAttachments(){
+function getAttachments(confPageId, callback){
   var attachmentsURL = config.confConfig.apiPath + "/content/" + confPageId + "/child/attachment";
   requestAPI(confPageId, config, attachmentsURL, 'false', '', false, function(err,data1){
-    if(err) console.log(errColor(err));
+    if(err) {
+      callback(err, null);
+    }
     else{
       data1 = JSON.parse(data1);
       if(data1.results.length > 0){
-        async.eachSeries(data1.results, function iteratee(result, callback) {
+        async.eachSeries(data1.results, function iteratee(result, cb) {
         if (result) {
-          saveAttachment(result._links.download,result.title,callback);
+          saveAttachment(confPageId, result._links.download,result.title,cb);
         }
         });
       }
+      callback(null, data1);
     }
   });
 }
 
-function saveAttachment(attachmentLink, title, cb){
+function saveAttachment(confPageId, attachmentLink, title, callback){
   requestAPI(confPageId, config, attachmentLink, true, title, false, function(err,data){
     console.log(attachmentColor(data));
-    cb();
+    callback();
   });
 }
 
-function callChildPages(){
-  childUrl = config.confConfig.apiPath + "/content/" + confPageId + "/child/page";
+function callChildPages(confPageId){
+  var childUrl = config.confConfig.apiPath + "/content/" + confPageId + "/child/page";
   requestAPI(confPageId, config, childUrl, false, '', false, function(err,data){
-    data = JSON.parse(data);
-    if(data.results.length > 0){
-      async.eachSeries(data.results, function iteratee(result, callback) {
-      if (result) {
-        confPageId = result.id;
-        loop();
-        callback();
+    if(err) console.log(errColor(err));
+    else{
+      data = JSON.parse(data);
+        if(data.results.length > 0){
+          for (var index in data.results){
+            loop(data.results[index].id);
+          }
+        }
       }
-      });
-    }
   });
 }
